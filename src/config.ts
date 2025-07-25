c9df9fdd6669d5d044643955bc089e7b5949adfa
Andrew <zeek@tuta.com>
2025-07-25 19:20:32 +0700

    FIX: dbHost field added to config; postgres conn in docker fix

--- src/config.ts
+++ src/config.ts
@@ -5,6 +5,7 @@ export const config = {
   port: Number(process.env.PORT) || 3000,
   jwtPrivateKeyPath: process.env.JWT_PRIVATE_KEY_PATH || '',
   jwtPublicKeyPath: process.env.JWT_PUBLIC_KEY_PATH || '',
+  dbHost: process.env.DATABASE_HOST || '',
   dbUser: process.env.DATABASE_USER || '',
   dbPassword: process.env.DATABASE_PASSWORD || '',
   dbName: process.env.DATABASE_NAME || '',
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import Fastify from 'fastify';
 import type { FastifyInstance } from 'fastify';
+import { Pool } from 'pg';
 import { config } from './config';
 import { initAuthRoutes } from './routes/auth.routes';
 import { initUserRoutes } from './routes/user.routes';
@@ -12,10 +13,9 @@ import { fastifyHelmet } from '@fastify/helmet';
 import cors from '@fastify/cors';
 import registerJwtPlugin from './plugins/jwt.plugin';
 import { errorHandler } from './common/app.error';
-import { Pool } from 'pg';
 import { setupTracing } from './common/tracer.otel';
 
-// setupTracing('api_service');
+setupTracing('api_service');
 
 async function main() {
   const server: FastifyInstance = Fastify({
@@ -23,6 +23,7 @@ async function main() {
   });
 
   const pgPool = new Pool({
+    host: config.dbHost,
     user: config.dbUser,
     password: config.dbPassword,
     database: config.dbName,
@@ -47,7 +48,7 @@ async function main() {
 
   server.setErrorHandler(errorHandler);
 
-  server.listen({ port: config.port }, function (err, address) {
+  server.listen({ host: '0.0.0.0', port: config.port }, function (err, address) {
     if (err) {
       server.log.error(err);
       process.exit(1);
