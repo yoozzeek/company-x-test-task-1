9bc6416da462aedbd04225ff61f692505a32c7fc
Andrew <zeek@tuta.com>
2025-07-28 14:33:28 +0700

    FIX: Global config accessible via getter instead of direct var

--- src/config.ts
+++ src/config.ts
@@ -1,18 +1,24 @@
 import dotenv from 'dotenv';
-dotenv.config();
 
-export const config = {
-  appPort: Number(process.env.APP_PORT) || 3000,
-  appHost: process.env.APP_HOST || 'localhost',
-  appName: process.env.APP_NAME || 'api_service',
-  jwtSecretKey: process.env.JWT_SECRET_KEY || undefined,
-  jwtPrivateKeyPath: process.env.JWT_PRIVATE_KEY_PATH || undefined,
-  jwtPublicKeyPath: process.env.JWT_PUBLIC_KEY_PATH || undefined,
-  autoMigrate: process.env.AUTO_MIGRATE || false,
-  migrationsTable: process.env.MIGRATIONS_TABLE || 'pgmigrations',
-  migrationsDir: process.env.MIGRATIONS_DIR || './migrations',
-  dbHost: process.env.DATABASE_HOST || '',
-  dbUser: process.env.DATABASE_USER || '',
-  dbPassword: process.env.DATABASE_PASSWORD || '',
-  dbName: process.env.DATABASE_NAME || '',
-};
+dotenv.config({
+  path: process.env.NODE_ENV === 'test' ? `.env.test` : `.env`,
+});
+
+export function getConfig() {
+  return {
+    appPort: Number(process.env.APP_PORT) || 3000,
+    appHost: process.env.APP_HOST || 'localhost',
+    appName: process.env.APP_NAME || 'api_service',
+    jwtSecretKey: process.env.JWT_SECRET_KEY || undefined,
+    jwtPrivateKeyPath: process.env.JWT_PRIVATE_KEY_PATH || undefined,
+    jwtPublicKeyPath: process.env.JWT_PUBLIC_KEY_PATH || undefined,
+    autoMigrate: process.env.AUTO_MIGRATE || false,
+    migrationsTable: process.env.MIGRATIONS_TABLE || 'pgmigrations',
+    migrationsDir: process.env.MIGRATIONS_DIR || './migrations',
+    dbHost: process.env.DATABASE_HOST || '',
+    dbPort: Number(process.env.DATABASE_PORT) || 5432,
+    dbUser: process.env.DATABASE_USER || '',
+    dbPassword: process.env.DATABASE_PASSWORD || '',
+    dbName: process.env.DATABASE_NAME || '',
+  };
+}
